c1c9e57b680178df85e33cc66de46a77c28d94c5
mdestafadilah <desta.08b@gmail.com>
2021-12-27 15:16:15 +0700

    updates

--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -16,6 +16,7 @@ class UsersModel extends Model
     protected $protectFields    = true;
     protected $allowedFields    = [
         "username",
+        "email",
         "password",
         "level"
     ];
@@ -35,12 +36,49 @@ class UsersModel extends Model
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
+    protected $beforeInsert   = ['beforeInsert'];
     protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
+    protected $beforeUpdate   = ['beforeUpdate'];
     protected $afterUpdate    = [];
     protected $beforeFind     = [];
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    protected function beforeInsert(array $data): array
+    {
+        return $this->getUpdatedDataWithHashedPassword($data);
+    }
+
+    protected function beforeUpdate(array $data): array
+    {
+        return $this->getUpdatedDataWithHashedPassword($data);
+    }
+
+    private function getUpdatedDataWithHashedPassword(array $data): array
+    {
+        if (isset($data['data']['password'])) {
+            $plaintextPassword = $data['data']['password'];
+            $data['data']['password'] = $this->hashPassword($plaintextPassword);
+        }
+        return $data;
+    }
+
+    private function hashPassword(string $plaintextPassword): string
+    {
+        return password_hash($plaintextPassword, PASSWORD_BCRYPT);
+    }
+                                      
+    public function findUserByEmailAddress(string $emailAddress)
+    {
+        $user = $this
+            ->asArray()
+            ->where(['email' => $emailAddress])
+            ->first();
+
+        if (!$user) 
+            throw new Exception('User does not exist for specified email address');
+
+        return $user;
+    }
 }
