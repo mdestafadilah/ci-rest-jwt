8b5ea06cef3ef45d1660e45c0ed47f4d60d165da
mdestafadilah <desta.08b@gmail.com>
2021-12-27 14:19:46 +0700

    Done.

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -24,11 +24,129 @@ class Users extends ResourceController
                 "required" => "password is required"
             ],
         ];
+
+        if (!$this->validate($rules,$messages)) {
+            $res = [
+                'status' => 500,
+                'error' => true,
+                'message' => $this->validator->getErrors(),
+                'data' => []
+            ];
+        }else{
+            $usersModel = new UsersModel();
+
+            $data = [
+                "username" => $this->request->getVar("username"),
+                "password" => $this->request->getVar("password")
+            ];
+
+            if ($usersModel->insert($data)) {
+                $res = [
+                    'status' => 200,
+                    'error' => false,
+                    'messages' => 'Berhasil, User ditambahkan',
+                    'data' => []
+                ];
+            } else {
+                $res = [
+                    'status' => 500,
+                    'error' => true,
+                    'messages' => 'Gagal menambahkan user',
+                    'data' => []
+                ];
+            }
+        }
+        return $this->respondCreated($res);
+    }
+
+    private function getKey()
+    {
+        return "my_secreet_codeigniter_4";
     }
 
     public function login()
     {
-        #TODO:
+        $rules = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        $messages = [
+            "username" => [
+                "required" => "Username required",
+            ],
+            "password" => [
+                "required" => "password is required"
+            ],
+        ];
+
+        if(!$this->validate($rules, $messages)) {
+            $res = [
+                'status' => 500,
+                'error' => true,
+                'messages' => $this->validator->getErrors(),
+                'data' => []
+            ];
+            return $this->respondCreated($res);
+
+        } else {
+            $usersModel = new UserModel();
+
+            $userData = $usersModel->where("username", $this->request->getVar("username"))->first();
+
+            if (!empty($userData)) {
+                if (password_verify($this->request->getVar("password"), $userdata['password'])) {
+
+                    $key = $this->getKey();
+
+                    $iat = time();
+                    $nbf = $iat + 10;
+                    $exp = $iat + 3600;
+
+                    $payLoad = [
+                        'iss' => 'the_claim',
+                        'aud' => 'the_aud',
+                        'iat' => $iat,
+                        'nbf' => $nbf,
+                        'exp' => $exp,
+                        'data' => $userData
+                    ];
+
+                    $token = JWT::encode($payLoad, $key);
+
+                    $res = [
+                        'status' => 200,
+                        'error' => false,
+                        'messages' => 'Berhasil Masuk',
+                        'data' => [
+                            'token' => $token
+                        ]
+                    ];
+
+                    return $this->respondCreated($res);
+
+                } else {
+                    $res = [
+                        'status' => 500,
+                        'error' => true,
+                        'messages' => 'Kesalahan details',
+                        'data' => []
+                    ];
+
+                    return $this->respondCreated($res);
+                }
+            } else {
+                $res = [
+                    'status' => 500,
+                    'error' => true,
+                    'messages' => 'User tidak ditemukan',
+                    'data' => []
+                ];
+                return $this->respondCreated($res);
+            }
+
+        }
+
     }
 
     public function details()
