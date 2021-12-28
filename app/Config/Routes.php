3a68eac2815af75a5166f7397230ddf527b1e6e3
mdestafadilah <desta.08b@gmail.com>
2021-12-28 15:19:39 +0700

    Create Client Controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,6 +39,12 @@ $routes->group("api", function ($routes) {
     $routes->get("profile", "Users::details");
 });
 
+$routes->get('client', 'Client::index');
+$routes->post('client', 'Client::store');
+$routes->get('client/(:num)', 'Client::show/$1');
+$routes->post('client/(:num)', 'Client::update/$1');
+$routes->delete('client/(:num)', 'Client::destroy/$1');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
